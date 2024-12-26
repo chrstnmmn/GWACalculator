01fa3a2d8a5f00113b7c84cf3c198bc26858c855
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2024-12-26 12:12:49 +0800

    adding functionatility to the website

--- JS/Compute.js
+++ JS/Compute.js
@@ -1,16 +1,26 @@
-let course = {
-  courseName:  'Chemistry',
-  Prelim: 80.25,
-  Midterm: 77.00,
-  Prefinal: 76.50,
-  Final: 77.38
-};
+const microPercent = 0.2;
+const macroPercent = 0.4;
 
-const percent = {
-  microPercent: .20,
-  macroPercent: .40
-};
+const courseName = document.getElementById("courseInput").value;
+const courseUnit = document.getElementById("courseUnit").value;
+const prelim = document.getElementById("prelimInput").value;
+const midterm = document.getElementById("midtermInput").value;
+const prefinal = document.getElementById("prefinalInput").value;
+const final = document.getElementById("finalInput").value;
 
-let result = course.Prelim * percent.microPercent
+function computeOverall(term1, term2, term3, term4) {
+	if (isNaN(prelim) || isNaN(midterm) || isNaN(prefinal) || isNaN(final)) {
+		console.log("you can only input numeric number!");
+	} else {
+		parseFloat(prelim, midterm, prefinal, final, courseUnit);
 
-console.log(result);
+		const total1 = term1 * microPercent;
+		const total2 = term2 * microPercent;
+		const total3 = term3 * microPercent;
+		const total4 = term4 * macroPercent;
+
+		const totalGrade = total1 + total2 + total3 + total4;
+
+		alert("Course name: " + courseName + "\nCourse unit: " + courseUnit + "\nYour average: " + totalGrade);
+	}
+}

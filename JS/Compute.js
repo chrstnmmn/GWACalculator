eb95fc593f2e974585e026fa7ca931e4ddda0947
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2024-12-27 12:26:08 +0800

    adding more features to my calculator

--- JS/Compute.js
+++ JS/Compute.js
@@ -1,41 +1,56 @@
-const microPercent = 0.2;
-const macroPercent = 0.4;
-
 function computeOverall() {
-	const courseName = document.getElementById("courseInput").value;
-	const courseUnit = parseFloat(document.getElementById("courseUnit").value);
-	const prelim = parseFloat(document.getElementById("prelimInput").value);
-	const midterm = parseFloat(document.getElementById("midtermInput").value);
-	const prefinal = parseFloat(document.getElementById("prefinalInput").value);
-	const final = parseFloat(document.getElementById("finalInput").value);
+	const Percent = {
+		microPercent: 0.2,
+		macroPercent: 0.4,
+	};
+	const CourseDetail = {
+		courseName: document.getElementById("courseInput").value,
+		courseUnit: parseFloat(document.getElementById("courseUnit").value),
+		prelim: parseFloat(document.getElementById("prelimInput").value),
+		midterm: parseFloat(document.getElementById("midtermInput").value),
+		prefinal: parseFloat(document.getElementById("prefinalInput").value),
+		final: parseFloat(document.getElementById("finalInput").value),
+	};
+
 	if (
-		isNaN(prelim) ||
-		isNaN(midterm) ||
-		isNaN(prefinal) ||
-		isNaN(final) ||
-		isNaN(courseUnit)
+		isNaN(CourseDetail.prelim) ||
+		isNaN(CourseDetail.midterm) ||
+		isNaN(CourseDetail.prefinal) ||
+		isNaN(CourseDetail.final) ||
+		isNaN(CourseDetail.courseUnit)
 	) {
 		alert("You can only input numeric values!");
 		clearForms();
 		return; // Exit the function if validation fails
 	}
 
-	const total1 = prelim * microPercent;
-	const total2 = midterm * microPercent;
-	const total3 = prefinal * microPercent;
-	const total4 = final * macroPercent;
+	const ComputeTotal = {
+		total1: CourseDetail.prelim * Percent.microPercent,
+		total2: CourseDetail.midterm * Percent.microPercent,
+		total3: CourseDetail.prefinal * Percent.microPercent,
+		total4: CourseDetail.final * Percent.macroPercent,
+	};
 
-	const totalGrade = total1 + total2 + total3 + total4;
+	const totalGrade =
+		ComputeTotal.total1 +
+		ComputeTotal.total2 +
+		ComputeTotal.total3 +
+		ComputeTotal.total4;
 
-	alert(
-		"Course name: " +
-			courseName +
-			"\nCourse unit: " +
-			courseUnit.toFixed(2) +
-			"\nYour average: " +
-			totalGrade.toFixed(2)
-	);
+	document.getElementById("TitleResult").innerText = CourseDetail.courseName;
+	document.getElementById("PrelimResult").innerText =
+		CourseDetail.prelim.toFixed(2);
+	document.getElementById("MidtermResult").innerText =
+		CourseDetail.midterm.toFixed(2);
+	document.getElementById("PrefinalResult").innerText =
+		CourseDetail.prefinal.toFixed(2);
+	document.getElementById("FinalResult").innerText =
+		CourseDetail.final.toFixed(2);
+	document.getElementById("overAllResult").innerText = totalGrade.toFixed(2);
 	clearForms();
+
+	const viewResult = document.getElementById("viewResult");
+	viewResult.scrollIntoView({behavior: "smooth", block: "start", inline: "center"});
 }
 
 function clearForms() {
@@ -45,6 +60,16 @@ function clearForms() {
 	document.getElementById("midtermInput").value = "";
 	document.getElementById("prefinalInput").value = "";
 	document.getElementById("finalInput").value = "";
-	location.reload();
 }
-  
+
+function removeCourse() {
+	document.getElementById("TitleResult").innerText = "Course or Subject Name";
+	document.getElementById("PrelimResult").innerText = "00.00";
+	document.getElementById("MidtermResult").innerText = "00.00";
+	document.getElementById("PrefinalResult").innerText = "00.00";
+	document.getElementById("FinalResult").innerText = "00.00";
+	document.getElementById("overAllResult").innerText = "00.00";
+
+	const viewResult = document.getElementById("AddCourse");
+	viewResult.scrollIntoView({behavior: "smooth", block: "end", inline: "end"});
+}
